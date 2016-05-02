ac5f5bc5011fcc4cf2ac4c4c7be382c2c691e3b4
Rodrigo Crespo <rcrespocepeda@gmail.com>
2016-05-02 13:57:40 +0200

    connect to ble

--- js/connect.js
+++ js/connect.js
@@ -8,7 +8,49 @@ window.onload = function() {
     });
 
     var connectPage = document.querySelector('#connect');
-
     connectPage.addEventListener("click", function() {
     });
+    
+    var BLEconnect = document.querySelector("#BLEconnect");
+    BLEconnect.addEventListener("click", connectBLE, false);
 };
+
+var adapter = tizen.bluetooth.getLEAdapter();
+var remoteDevice = null;
+var watchId;
+
+var connectionListener = 
+{
+   onconnected: function(device) 
+   {
+      console.log("Connected to the device: " + device.name + " [" + device.address + "]");
+   },
+   ondisconnected: function(device) 
+   {
+      console.log("Disconnected from the device " + device.name + " [" + device.address + "]");
+   }
+};
+
+function onDeviceFound(device)
+{
+   if (remoteDevice === null) 
+   {
+      remoteDevice = device;
+      console.log("Found device " + device.name + ". Connecting...");
+
+      watchId = remoteDevice.addConnectStateChangeListener(connectionListener);
+
+      remoteDevice.connect();
+   }
+
+   adapter.stopScan();
+}
+
+var connectBLE = function(){
+	console.log("start connect BLE scan");
+	adapter.startScan(onDeviceFound);
+
+};
+
+
+
